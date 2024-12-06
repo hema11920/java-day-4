298035290b2e0f3c56381e674e70c28bd0bff08f
hema11920 <kasireddygarihemasree@gmail.com>
2024-12-06 09:34:31 +0530

    Add files via upload

---
+++ printingnaturalnum.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class printingnaturalnum{
+    public static void main(String args[]){
+        int n=10;
+        int i=n-2;
+        while(i>=0)
+        {
+            System.out.println(n-i);
+            i-=1;
+        }
+    }
+}
